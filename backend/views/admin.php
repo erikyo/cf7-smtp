061a51b15a1a6166f47db676fb32151ddcfd762e
Erik Golinelli <erik@codekraft.it>
2024-02-11 20:07:34 +0100

    backend view page

--- backend/views/admin.php
+++ backend/views/admin.php
@@ -29,24 +29,28 @@
 		<h3><?php esc_html__( 'Options', CF7_SMTP_TEXTDOMAIN ); ?></h3>
 		<form method="post" action="options.php" id="cf7-smtp-settings" class="form-table">
 			<?php
+
 			/* This prints out all hidden setting fields */
 			settings_fields( CF7_SMTP_TEXTDOMAIN . '-settings' );
 
 			/* This prints out the smtp settings */
+			echo '<div class="card smtp-settings-options">';
 			do_settings_sections( 'smtp-settings' );
 			submit_button();
-			echo '<br>';
+			echo '</div>';
+
 			/* This prints the style options (template) */
+			echo '<div class="card smtp-style-options">';
 			do_settings_sections( 'smtp-style' );
 			submit_button();
-			echo '<br>';
+			echo '</div>';
+
 			/* This prints the cron options (mail report) */
+			echo '<div class="card main-options">';
 			do_settings_sections( 'smtp-cron' );
-			echo '<br>';
-			/*
-			 EDIT: Commented the undefined constant */
-			/* This Prints the CRON job for mail reports */
-			if ( wp_next_scheduled( 'cf7_smtp_report' ) ) {
+
+
+			if ( wp_next_scheduled( 'cf7_smtp_report' ) && CF7ANTISPAM_DEBUG ) {
 				echo '<div class="tip schedule"><h1>⏰</h1>';
 				printf(
 					'<small class="monospace"><b>%s</b> %s <br/><b>%s</b> %s</small>',
@@ -57,7 +61,29 @@
 				);
 				echo '</div>';
 			}
+
+
 			submit_button();
+			echo '</div>';
+
+
+			$cf7_smtp_report = get_option( 'cf7-smtp-report', false );
+
+			echo '<div class="card smtp-style-chart">';
+			echo '<h2>' . esc_html__( 'Stats', CF7_SMTP_TEXTDOMAIN ) . '</h2>';
+			if ( ! empty( $cf7_smtp_report ) ) {
+				echo '<h4>' . esc_html__( 'Mail vs Time', CF7_SMTP_TEXTDOMAIN ) . '</h4>';
+				echo '<canvas id="line-chart" width="480" height="250"></canvas>';
+				echo '<hr>';
+				echo '<h4>' . esc_html__( 'Mail sent vs Mail failed', CF7_SMTP_TEXTDOMAIN ) . '</h4>';
+				echo '<canvas id="pie-chart" width="200" height="250"></canvas>';
+
+				echo '<script id="smtpReport">var smtpReportData =' . wp_json_encode( $cf7_smtp_report ) . '</script>';
+			} else {
+				echo '<span class="chart-icon">📊</span>';
+				echo '<h4 class="no-chart-title">' . esc_html__( 'No email sent (yet)', CF7_SMTP_TEXTDOMAIN ) . '</h4>';
+			}
+			echo '</div>';
 			?>
 		</form>
 	</div>
