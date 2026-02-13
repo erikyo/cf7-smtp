cfacfce065b704a7988d7835c67181604c4d22f1
Erik Golinelli <erik@codekraft.it>
2026-02-13 23:58:55 +0100

    Add `CF7_SMTP_OAuthProvider` implementation: custom OAuth token retrieval for PHPMailer integration

---
+++ core/OAuthProvider.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * CF7_SMTP OAuth Provider
+ *
+ * @package   cf7_smtp
+ * @author    Erik Golinelli <erik@codekraft.it>
+ * @copyright 2022 Erik
+ * @license   GPL 2.0+
+ * @link      https://modul-r.codekraft.it/
+ */
+
+use PHPMailer\PHPMailer\Exception;
+
+require_once __DIR__ . '/OAuthTokenProvider.php';
+
+/**
+ * Custom OAuth Token Provider for PHPMailer
+ * Implements CF7_SMTP_OAuthTokenProvider interface using League OAuth2 Client
+ */
+class CF7_SMTP_OAuthProvider implements CF7_SMTP_OAuthTokenProvider {
+	/**
+	 * The OAuth provider instance
+	 *
+	 * @var object
+	 */
+	private object $provider;
+
+	/**
+	 * The client ID
+	 *
+	 * @var string
+	 */
+	private string $client_id;
+
+	/**
+	 * The client secret
+	 *
+	 * @var string
+	 */
+	private string $client_secret;
+
+	/**
+	 * The refresh token used to obtain new access tokens
+	 *
+	 * @var string
+	 */
+	private string $refresh_token;
+
+	/**
+	 * Constructor
+	 *
+	 * @param object $provider The OAuth provider instance.
+	 * @param string $client_id The client ID.
+	 * @param string $client_secret The client secret.
+	 * @param string $refresh_token The refresh token.
+	 */
+	public function __construct( object $provider, string $client_id, string $client_secret, string $refresh_token ) {
+		$this->provider      = $provider;
+		$this->client_id     = $client_id;
+		$this->client_secret = $client_secret;
+		$this->refresh_token = $refresh_token;
+	}
+
+	/**
+	 * Get the OAuth64 string for PHPMailer
+	 *
+	 * @return string The OAuth64 string
+	 * @throws Exception If the OAuth provider or refresh token is not configured.
+	 */
+	public function get_oauth64(): string {
+		if ( ! $this->provider || ! $this->refresh_token ) {
+			throw new Exception( 'OAuth provider or refresh token not configured.' );
+		}
+
+		try {
+			$new_access_token = $this->provider->getAccessToken(
+				'refresh_token',
+				array(
+					'refresh_token' => $this->refresh_token,
+				)
+			);
+
+			// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
+			return base64_encode(
+				'user=' . $new_access_token->getResourceOwnerId()
+									. '\001auth=Bearer ' . $new_access_token->getToken()
+									. '\001\001'
+			);
+		} catch ( Exception $e ) {
+			throw new Exception( 'OAuth token retrieval failed: ' . esc_html( $e->getMessage() ) );
+		}
+	}
+}
